6464a1e89a1b1df1f54d0a9f571a5ce534de22f2
rahhh <244263772+prodbylyra@users.noreply.github.com>
2026-02-02 16:55:21 +1000

    doc(validator): document reflection test

--- src/main/java/us/prodbylyra/obfuscators/validator/test/impl/reflection/ReflectionTest.java
+++ src/main/java/us/prodbylyra/obfuscators/validator/test/impl/reflection/ReflectionTest.java
@@ -6,6 +6,16 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
+/**
+ *
+ * @author lyra (@prodbylyra)
+ *
+ * <p>
+ * A reflection test. Uses reflection to test whether the obfuscator is capable of
+ * properly handling indirect method invocation.
+ * </p>
+ *
+ **/
 public class ReflectionTest implements Test {
     @Override
     public String type() {
@@ -24,6 +34,10 @@ public class ReflectionTest implements Test {
                             MethodType.methodType(long.class, long.class)
                     );
 
+            // -
+            // The `invokeExact` method often breaks obfuscators and especially jni use due to its
+            // polymorphic signature.
+            // -
             result = (long) methodHandle.invokeExact(1337L);
         } catch (Throwable t) {
             throw new IllegalStateException("Failed reflection method invocation", t);
